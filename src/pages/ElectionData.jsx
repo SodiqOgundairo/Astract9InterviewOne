4d129c52b52b7b28bc8896a43558f45f385702ce
Opeyemi Ogundairo <kingmclamba1@gmail.com>
2023-03-03 16:55:57 +0100

    routes updated

--- src/pages/ElectionData.jsx
+++ src/pages/ElectionData.jsx
@@ -1,7 +1,22 @@
 const ElectionData = () => {
   return (
-    <div  className='text-white mt-[80px] mx-4 md:mx-[40px]  z-40'>
-      <p className='font-[32px]'>Election Data</p>
+    <div  className='text-white mt-[120px] mx-4 md:mx-[40px]  z-40'>
+      <div className='flex flex-col md:flex-row'>
+      <p className='font-bold text-3xl  leading-none'>Election Data</p>
+      <p className='bg-[#181C2E] border-[#242B47] items-center md:mx-3 px-[8px] py-[1.3px] rounded-[4px] w-fit' >
+        <span className='text-sm '>PRESIDENTIAL</span> 
+        <span className='opacity-30'> / </span> 
+        <span className='text-sm '>2023</span> 
+      </p>
+      </div>
+      <div className='mt-[120px] flex flex-col md:flex-row justify-between'>
+      <p className='font-medium text-2xl'>Election Data</p>
+      <p className='bg-[#181C2E] border-[#242B47]  leading-none md:mx-3 px-[8px] py-[1.3px] rounded-[4px] w-fit' >
+        <span className='text-sm '>PRESIDENTIAL</span> 
+        <span className='opacity-30'> / </span> 
+        <span className='text-sm '>2023</span> 
+      </p>
+      </div>
     </div>
   )
 }
